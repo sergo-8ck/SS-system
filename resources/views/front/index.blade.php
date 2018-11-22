f86ed057c100574252bcb78aa711bcb3707275b2
sergo-8ck <sergo-8ck@yandex.ru>
2018-11-22 23:42:21 +0300

    view vizitka

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -3,14 +3,14 @@
 <div class="theme-inner-banner section-spacing">
   <div class="overlay">
     <div class="container">
-      <h2>ABOUT</h2>
+      <h2>Сайт системы сертификации</h2>
     </div>
   </div>
 </div>
 <div class="callout-banner no-bg">
   <div class="container clearfix">
-    <h3 class="title">High-Quality <br> Market Experinces</h3>
-    <p>A tale of a fateful trip that started from this tropic port aboard this tiny ship today still wanted by the government they survive as soldiers of fortune to ever wondered the east side to a deluxe apartment.</p>
+    <h3 class="title">Высокое качество <br> Эксперименты</h3>
+    <p>Сказка о роковой поездке, которая началась с этого тропного порта на борту этого крошечного корабля, который по-прежнему нужен правительству, выживет, когда солдаты удачи когда-либо задумывались о восточной стороне роскошной квартиры.</p>
     <a href="#" class="theme-button-one">CONTACT US</a>
   </div>
 </div>
@@ -21,10 +21,10 @@
       <div class="row">
         <div class="col-lg-6 col-12 text order-lg-last">
           <div class="theme-title-one">
-            <h2>SHORT HISTORY</h2>
+            <h2>КРАТКАЯ ИСТОРИЯ</h2>
           </div> <!-- /.theme-title-one -->
-          <p>A tale of a fateful trip that started from this tropic port aboard this tiny ship today still wanted by the government they survive as soldiers of fortune to a deluxe you apartment in the sky to explore strange new worlds to seek out new life and new civilizations to boldly go where no man has gone.</p>
-          <p>You would see the biggest gift would be from me and the card attached would so thank you for being a friend the biggest gift.</p>
+          <p>Сказка о роковой поездке, которая началась с этого тропного порта на борту этого крошечного корабля, который по-прежнему хочет, чтобы правительство выжило, как солдат удачи, чтобы позволить вам квартиру в небе, чтобы исследовать странные новые миры, чтобы искать новую жизнь и новые цивилизации, чтобы смело идти туда, куда ни один человек не ушел.</p>
+          <p>Вы увидите, что самый большой подарок будет от меня, и прилагаемая карта будет так благодарна вам за то, что вы друг, самый большой подарок.</p>
           <img src="images/home/sign-black.png" alt="" class="sign">
         </div> <!-- /.col- -->
         <div class="col-lg-6 col-12 order-lg-first">
@@ -39,8 +39,8 @@
   <div class="overlay">
     <div class="container">
       <div class="theme-title-one">
-        <h2>WE ARE ALWAYS BEST</h2>
-        <p>A tale of a fateful trip that started from this tropic port aboard this tiny ship today stillers</p>
+        <h2>МЫ ЛУЧШЕ ВО ВСЕМ</h2>
+        <p>Сказка о роковой поездке, которая началась с этого тропического порта на борту этого крошечного корабля сегодня,</p>
       </div> <!-- /.theme-title-one -->
 
       <div class="wrapper row no-gutters">
@@ -49,18 +49,18 @@
           <ul class="best-list-item">
             <li>
               <i class="icon flaticon-puzzle"></i>
-              <h5><a href="#">Best Strategy</a></h5>
-              <p>The Love Boat soon will be making another run plore strange tools enter new worlds.</p>
+              <h5><a href="#">Лучшая стратегия</a></h5>
+              <p>Влюбленная лодка скоро станет еще одним бегом, странные инструменты войдут в новые миры.</p>
             </li>
             <li>
               <i class="icon flaticon-presentation"></i>
-              <h5><a href="#">High-Quality Services</a></h5>
-              <p>The Love Boat soon will be making another run plore strange tools enter new worlds.</p>
+              <h5><a href="#">Высококачественные услуги</a></h5>
+              <p>Влюбленная лодка скоро станет еще одним бегом, странные инструменты войдут в новые миры.</p>
             </li>
             <li>
               <i class="icon flaticon-people"></i>
-              <h5><a href="#">Friendly Support</a></h5>
-              <p>The Love Boat soon will be making another run plore strange tools enter new worlds.</p>
+              <h5><a href="#">Дружественная поддержка</a></h5>
+              <p>Влюбленная лодка скоро станет еще одним бегом, странные инструменты войдут в новые миры.</p>
             </li>
           </ul>
         </div>
@@ -70,45 +70,11 @@
 </div>
 
 
-<div class="theme-counter-two section-spacing">
-  <div class="container">
-    <div class="clearfix">
-      <div class="cunter-wrapper">
-        <div class="row">
-          <div class="col-md-3 col-6">
-            <div class="single-counter-box">
-              <div class="number"><span class="timer" data-from="0" data-to="30" data-speed="1200" data-refresh-interval="5">0</span>+</div>
-              <p>Years of Excellence</p>
-            </div>
-          </div>
-          <div class="col-md-3 col-6">
-            <div class="single-counter-box">
-              <div class="number"><span class="timer" data-from="0" data-to="100" data-speed="1200" data-refresh-interval="5">0</span>%</div>
-              <p>Client Satisfaction</p>
-            </div>
-          </div>
-          <div class="col-md-3 col-6">
-            <div class="single-counter-box">
-              <div class="number"><span class="timer" data-from="0" data-to="53" data-speed="1200" data-refresh-interval="5">0</span>k</div>
-              <p>Cases Completed</p>
-            </div>
-          </div>
-          <div class="col-md-3 col-6">
-            <div class="single-counter-box">
-              <div class="number"><span class="timer" data-from="0" data-to="24" data-speed="1200" data-refresh-interval="5">0</span></div>
-              <p>Consultants</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
 
 <div class="core-values">
   <div class="container">
     <div class="theme-title-one">
-      <h2>CORE VALUES</h2>
+      <h2>ГЛАВНЫЕ СОБЫТИЯ</h2>
     </div>
     <div class="wrapper">
       <div class="core-value-slider">
@@ -116,11 +82,10 @@
           <div class="single-value-block">
             <div class="image-box">
               <img src="images/home/17.jpg" alt="">
-              <div class="overlay"><a href="#" class="theme-button-one">READ MORE</a></div>
             </div> <!-- /.image-box -->
             <div class="text">
-              <h5><a href="#">Challenging Staff</a></h5>
-              <p>The Love Boat soon will be making other run plore strange tools.</p>
+              <h5><a href="#">Оспаривающий персонал</a></h5>
+              <p>Влюбленная лодка скоро будет делать другие пустые странные инструменты.</p>
             </div>
           </div>
         </div>
@@ -128,11 +93,10 @@
           <div class="single-value-block">
             <div class="image-box">
               <img src="images/home/18.jpg" alt="">
-              <div class="overlay"><a href="#" class="theme-button-one">READ MORE</a></div>
             </div>
             <div class="text">
-              <h5><a href="#">Integrity Properties</a></h5>
-              <p>The Love Boat soon will be making other run plore strange tools.</p>
+              <h5><a href="#">Свойства целостности</a></h5>
+              <p>Влюбленная лодка скоро будет делать другие пустые странные инструменты.</p>
             </div>
           </div>
         </div>
@@ -140,11 +104,10 @@
           <div class="single-value-block">
             <div class="image-box">
               <img src="images/home/19.jpg" alt="">
-              <div class="overlay"><a href="#" class="theme-button-one">READ MORE</a></div>
             </div>
             <div class="text">
-              <h5><a href="#">Innovation Solutions</a></h5>
-              <p>The Love Boat soon will be making other run plore strange tools.</p>
+              <h5><a href="#">Инновационные решения</a></h5>
+              <p>Влюбленная лодка скоро будет делать другие пустые странные инструменты.</p>
             </div>
           </div>
         </div>
@@ -157,7 +120,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-3 col-sm-4 col-12">
-        <h6>OUR <br>PARTNERS</h6>
+        <h6>НАШИ <br>ПАРТНЕРЫ</h6>
       </div>
       <div class="col-md-9 col-sm-8 col-12">
         <div class="partner-slider">
--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -7,15 +7,15 @@
         <ul>
           <li class="address">
             <i class="icon flaticon-placeholder"></i>
-            <h6>Address:</h6>
-            <p>2A0, Queenstown St, USA.</p>
+            <h6>Адрес:</h6>
+            <p>ул. Буракова, д. 27 корп. 3.</p>
           </li>
           <li class="address">
             <i class="icon flaticon-multimedia"></i>
-            <h6>Mail us:</h6>
-            <p>supporthere@mail.com</p>
+            <h6>E-mail адрес:</h6>
+            <p>ATG-Service@yandex.ru</p>
           </li>
-          <li class="quotes"><a href="#">GET A QUOTES</a></li>
+          <li class="quotes"><a href="#">Авторизация</a></li>
         </ul>
       </div>
     </div>
@@ -28,34 +28,33 @@
           <nav id="mega-menu-holder" class="clearfix">
             <ul class="clearfix">
               <li><a href="/">Главная</a></li>
-              <li><a href="/details">Детали</a></li>
-              <li><a href="contact">contact</a></li>
+              <li><a href="contact">Контакты</a></li>
             </ul>
           </nav>
         </div>
 
         <div class="right-widget float-right">
           <ul>
-            <li class="social-icon">
-              <ul>
-                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-              </ul>
-            </li>
+            {{--<li class="social-icon">--}}
+              {{--<ul>--}}
+                {{--<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>--}}
+                {{--<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>--}}
+                {{--<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>--}}
+                {{--<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>--}}
+              {{--</ul>--}}
+            {{--</li>--}}
             <li class="cart-icon">
-              <a href="#"><i class="flaticon-tool"></i> <span>2</span></a>
+              <a href="#"><i class="flaticon-login"></i></a>
             </li>
-            <li class="search-option">
-              <div class="dropdown">
-                <button type="button" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-search" aria-hidden="true"></i></button>
-                <form action="#" class="dropdown-menu">
-                  <input type="text" Placeholder="Enter Your Search">
-                  <button><i class="fa fa-search"></i></button>
-                </form>
-              </div>
-            </li>
+            {{--<li class="search-option">--}}
+              {{--<div class="dropdown">--}}
+                {{--<button type="button" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-search" aria-hidden="true"></i></button>--}}
+                {{--<form action="#" class="dropdown-menu">--}}
+                  {{--<input type="text" Placeholder="Enter Your Search">--}}
+                  {{--<button><i class="fa fa-search"></i></button>--}}
+                {{--</form>--}}
+              {{--</div>--}}
+            {{--</li>--}}
           </ul>
         </div>
       </div>
